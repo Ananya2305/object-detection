96578fb60854ee321d97307dfadcbd0c93bd8f5e
Ananya2305 <planetsneha.shukla@gmail.com>
2024-01-28 14:39:05 +0530

    changes

--- object-detection/main.js
+++ object-detection/main.js
@@ -1,48 +1,56 @@
-status='';
+status = '';
 
-img="";
-function preload(){
-    img=loadImage('dog_cat.jpg');
+img = "";
+object = [];
+
+function preload() {
+    img = loadImage('dog_cat.jpg');
 
 }
 
-function setup(){
-    canvas=createCanvas(640,420);
+function setup() {
+    canvas = createCanvas(640, 420);
     canvas.center();
-objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-document.getElementById("status").innerHTML="status:Detecting Objects";
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status:Detecting Objects";
 
 }
-function modelLoaded(){
+function modelLoaded() {
     console.log("modelLoaded");
-    status-true;
-    objectDetector.detect(img,gotResult);
+    status - true;
+    objectDetector.detect(img, gotResult);
 
-    
+
 }
-function gotResult(error,results){
-    if(error){
+function gotResult(error, results) {
+    if (error) {
         console.log(error);
 
     }
     console.log(results);
+    object = results;
+
 
 }
 
 
 
-function draw(){
-    image(img,0,0,640,420);
-    fill("#FF0000");
-    text("dog",45,75);
-noFill();
-stroke("#FF0000");
-rect(30,60,450,350);
-fill("#FF0000");
-text("cat",320,120);
-noFill();
-stroke("#FF0000");
-rect(300,90,270,320);
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+
+        for (i = 0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "status=objectDetected";
+            fill("#FF0000");
+            percent=floor(object[i].confidence*100);
+    
+            text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }
+
 
 
 }
